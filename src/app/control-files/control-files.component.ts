807c3a7434312f44db3fccaddb3fc03842c61e22
Oleh Kopylchak <oleg12311999@gmail.com>
2021-12-09 10:39:42 +0200

    Add bootstrap data table without back end linking. Split code into the components

---
+++ src/app/control-files/control-files.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-files',
+  templateUrl: './control-files.component.html',
+  styleUrls: ['./control-files.component.css']
+})
+export class ControlFilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
